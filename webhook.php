b1fcb47af143e8cce16add4b485b101fe723a4a6
Navdeep Singh <navdeep.er@gmail.com>
2018-12-20 11:53:47 +0530

    Update

--- webhook.php
+++ webhook.php
@@ -1,7 +1,7 @@
 <?php
 
 $challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub-verify_token'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
 if ($verify_token == 'abc123') {
   echo $challenge;
