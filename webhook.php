3694ebb57bff1d0ed5dd790f755802e443dab437
Navdeep Singh <navdeep.er@gmail.com>
2018-12-20 17:28:13 +0530

    Updated webhook

--- webhook.php
+++ webhook.php
@@ -6,3 +6,7 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token == 'falconagencyrocking123') {
   echo $challenge;
 }
+
+// Grab the data from post body
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
