f8955cea6c0ed314e6e6dbbad02747c7d2e3dccf
Navdeep Singh <navdeep.er@gmail.com>
2018-12-21 10:26:44 +0530

    Permissions updated

--- platform.php
+++ platform.php
@@ -47,7 +47,7 @@ window.fbAsyncInit = function() {
           ul.appendChild(li);
         }
       })
-    }, {scope: 'leads_retrieval'});
+    }, {scope: 'manage_pages, leads_retrieval'});
   }
 
 </script>
